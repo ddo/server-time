143e5cd6b6148a95349f8b1b79d3d299579711f8
Ddo <joeddo89@gmail.com>
2014-04-13 17:49:02 +0700

    remove test, add timestamp and timestamp in millisecond

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app     = express();
 
-app.get('/', function(req, res){
+app.get('/', function(r){
     var now = new Date();
 
     res.send({
         string: now.toUTCString(),
-        now: now,
-        timestamp: now.getTime()
+        timestamp: parseInt(now.getTime()/1000, 10),
+        timestamp_millisecond: now.getTime()
     });
 });
 
